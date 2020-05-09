36e7201b1028b731f465c17820d9dbcd1b1007a0
Natalia Lira <natalia.rlira@gmail.com>
2020-05-09 18:57:34 -0300

    renaming register to index + cleaning up the js code

--- js/script.js
+++ js/script.js
@@ -1,42 +1,15 @@
-/* declarando variáveis */
-let cep = document.getElementById("cep");
-let verificaCep = document.getElementById("verificaCep");
-let rua = document.getElementById("rua");
-let bairro = document.getElementById("bairro");
-
-/* configurando como a mensagem será recebida e interpretada */
-// let config = { 
-//     headers: {
-//         "Content-Type": "application/json"
-//     },
-//     method: "GET"
-// }
-
-/* consumindo api com JS Vanilla */
-// verificaCep.onclick = function (){
-//     fetch('https://viacep.com.br/ws/'+cep.value+'/json/', config)
-//     .then(function (response){
-//         return response.json();
-//     })
-//     .then(function (endereco){
-//         rua.value = endereco.logradouro;
-//         bairro.value = endereco.bairro;
-//     })
-// }
-
-/* resolvendo com JQuery */
+/* consuming api |consumo da api */
 $("#cep").focusout(
     function (){
-        /* validação se campocep foi preenchido (verificando número de caracteres por meio do atributo .length) */
+        /* checking if cep input has the normal brazilian zi-code length | validação se campo cep foi preenchido (verificando número de caracteres por meio do atributo .length) */
         if(this.value.length == 8){
-            /*consumo de api - assincrona
-            fazendo uso do elemento this (em JQuery precisamos usar $(this) e assim nos referimos ao elemento à quem recebeu o evento)*/
+            /* using this | fazendo uso do elemento this (em JQuery precisamos usar $(this) e assim nos referimos ao elemento à quem recebeu o evento)*/
             $.ajax({
                 url: 'https://viacep.com.br/ws/'+$(this).val()+'/json/',
                 type: 'get',
                 dataType: 'json',
                 success: function(response){
-                // podemos usar console.log(response) para verificar se há resposta antes de atribuírmos valores aos elementoss
+                //dica: podemos usar console.log(response) para verificar se há resposta e como está a resposta da api antes de atribuírmos valores aos elementos
                 $('#rua').val(response.logradouro) 
                 $('#bairro').val(response.bairro)
                 },
